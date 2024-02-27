b06ed15f8243839d8f41dc3bcfce687d3219ccd3
Pavel Sashko <89eightnine89@mail.ru>
2024-02-27 10:32:23 +0100

    loop

--- ie2-git/main.cpp
+++ ie2-git/main.cpp
@@ -4,5 +4,9 @@ int main()
 {
     std::cout << "Hello World!" << std::endl;
     std::cout << "dddd" << std::endl;
+        for(int i = 0;i < 10 ; i++){
+        cout << "Hello World";
+    }
+    
     return 0;
 }
